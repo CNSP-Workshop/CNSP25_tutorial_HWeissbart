df96dcb8baffc0ee7d3bad18bd943feb973f3107
Hugo Weissbart <hugo.weissbart@donders.ru.nl>
2025-09-02 12:48:01 +0200

    Enhance visualization utilities: add phase-amplitude distribution and signal plotting functions

--- utils/utils.py
+++ utils/utils.py
@@ -2,7 +2,26 @@ from tqdm.auto import tqdm
 import urllib.request
 import shutil
 import h5py
+import numpy as np
+from scipy.stats import zscore
 import os.path as op
+
+STORY_IDs = {
+    11: 'Anderson_S01_P01_normalized',
+    12: 'Anderson_S01_P02_normalized',
+    13: 'Anderson_S01_P03_normalized',
+    14: 'Anderson_S01_P04_normalized',
+    21: 'grimm_23_1_normalized',
+    22: 'grimm_23_2_normalized',
+    23: 'grimm_23_3_normalized',
+    31: 'grimm_20_1_normalized',
+    32: 'grimm_20_2_normalized',
+    41: 'EAUV_part1_normalized',
+    42: 'EAUV_part2_normalized',
+    51: 'ANGE_part1_normalized',
+    61: 'BALL_part1_normalized',
+}
+storynames = [s for s in STORY_IDs.values()]
 
 def download_file(url, save_path, chunk_size=1024):
     try:
@@ -26,6 +45,7 @@ def download_file(url, save_path, chunk_size=1024):
                 print(f"✅ File downloaded: {save_path}")
     except urllib.error.URLError as e:
         print(f"❌ Download failed, network error occurred: {e}")
+
 def list_h5_data(fullpath='../data/stim/predictors.hdf5', max_depth=None):
     """
     Describe the content of the h5 data file recursively up to a specified depth.
@@ -58,3 +78,97 @@ def list_h5_data(fullpath='../data/stim/predictors.hdf5', max_depth=None):
 
     with h5py.File(fullpath, 'r') as f:
         f.visititems(show_hierarchy)
+
+def read_h5_data(feat_type='acoustic',
+                 datadir='../data/stim', fname='predictors.hdf5',
+                 stories=storynames):
+    """
+    Read, for a given sampling rate and if available, the stimulus representation:
+    acoustic (continuous) or word-level data for all story parts.
+
+    Parameters
+    ----------
+    feat_type : str, optional
+        Type of stimulus representation. The default is 'acoustic'.
+    datadir : str, optional
+        Path to folder containing h5py file.
+    fname : str, optional
+        filename. The default is 'predictors.hdf5'.
+
+    Returns
+    -------
+    out : list<ndarray>
+        Numpy array of time aligned features for each story parts.
+
+    """
+    srate=100
+    transcript_key='transcripts_v2'
+    out = []
+    with h5py.File(op.join(datadir, fname), 'r') as f:
+        try:
+            for s in stories:
+                out.append(f[transcript_key][str(srate)][feat_type][s][()])
+        except KeyError:
+            raise KeyError(f"The required sampling rate ({srate}), feature type ({feat_type}) or {transcript_key} transcript family is not available")
+    return out
+
+def get_feature_signal(feats=['acoustic', 'wordonsets'],
+                       stories=storynames, normalise='all', verbose=True):
+    """
+    Extract the final design matrix from features data (as stored in HDF5 file).
+
+    Parameters
+    ----------
+    feats : list
+        Can contain any of ['acoustic', 'wordonsets', 'wordfrequency', 'surprisal', 'entropy',
+        'kl', 'PE', 'depth', 'close', 'open']
+    stories : list of str
+        Which stories to extract
+    normalise : str
+        'all' or 'story'. Whether to normalise per story or across all stories jointly.
+
+    Returns
+    -------
+    X : list of ndarray
+    """
+    import numpy as np
+    wl_feats_id = {f:k for f,k in zip(
+        ['acoustic', 'wordonsets', 'surprisal', 'entropy',
+        'kl', 'PE', 'wordfrequency', 'depth', 'close', 'open'], range(9)
+        )
+    }
+    srate=100
+    transcripts='transcripts_v2'
+    assert normalise in ['all', 'story'], "Normalisation must per story ('story') or across all stories ('all')"
+    if verbose: print("Load stimulus features")
+    envs = read_h5_data(feat_type='acoustic', stories=stories)
+    wordlevels = read_h5_data(feat_type='wordlevel', stories=stories)
+    if verbose: print(f"Loading feature signal for : {feats}")
+    X = []
+    for k, s in enumerate(stories):
+        x_ = []
+        for f in feats:
+            if f == 'acoustic':
+                x_.append(zscore(envs[k]))
+            else:
+                x_.append(wordlevels[k][:, wl_feats_id[f]])
+        X.append(np.vstack(x_).T)
+    # normalise feature to unit variance (so we do not remove the mean here...)
+    var = np.var(np.vstack(X), 0)
+    for x in X:
+        if normalise=='story':
+            x /= np.sqrt(np.var(x, 0)) # per story
+        elif normalise=='all':
+            x /= np.sqrt(var) # across all stories
+    if verbose: print(f"Done. X shape: {X[0].shape}")
+    return X
+
+def normalize_complex(z):
+    return z/np.abs(z)
+
+def check_complex(amp, phase):
+    if any(np.iscomplex(amp)):                                                                                                                              
+        amp = abs(amp)                                                                                                                                      
+    if any(np.iscomplex(phase)):                                                                                                                            
+        phase = np.angle(phase)  
+    return amp, phase
--- utils/visu.py
+++ utils/visu.py
@@ -34,3 +34,50 @@ def plot_fft(signal, fs, ax=None, scale='amplitude'):
     ax.set_title('FFT of the signal')
     ax.grid()
     return ax.figure
+
+def plot_phase_amp_distribution(bins, pmf, ax=None):
+    if ax is None:
+        f, ax = plt.subplots(1, 1)
+    b = ax.bar(bins, pmf, alpha=0.8, width=bins[1]-bins[0])
+    ax.margins(x=0)
+    ax.set_xticks(np.linspace(0, 2*np.pi, 5))
+    ax.set_xticklabels(['0', r'$\frac{\pi}{2}$', r'$\pi$', r'$3\frac{\pi}{2}$', r'$2\pi$'])
+    ax.grid(axis='x', which='both')
+    return b
+
+def plot_sig(t, signal_fast, env_fast, ax=None):
+    if ax is None:
+        f, ax = plt.subplots(1, 1)
+    l = ax.plot(t, np.asarray([signal_fast, env_fast]).T)
+    ax.margins(x=0)
+    ax.set_xlim([0, 1.5])
+    return l
+
+    
+def multiple_formatter(denominator=2, number=np.pi, latex='\\pi'):
+    def gcd(a, b):
+        while b:
+            a, b = b, a%b
+        return a
+    def _multiple_formatter(x, pos):
+        den = denominator
+        num = int(np.rint(den*x/number))
+        com = gcd(num,den)
+        (num,den) = (int(num/com),int(den/com))
+        if den==1:
+            if num==0:
+                return r'$0$'
+            if num==1:
+                return r'$%s$'%latex
+            elif num==-1:
+                return r'$-%s$'%latex
+            else:
+                return r'$%s%s$'%(num,latex)
+        else:
+            if num==1:
+                return r'$\frac{%s}{%s}$'%(latex,den)
+            elif num==-1:
+                return r'$\frac{-%s}{%s}$'%(latex,den)
+            else:
+                return r'$\frac{%s%s}{%s}$'%(num,latex,den)
+    return _multiple_formatter
